ea879feee16f582a353fb0a3f8824cc61c4de3c6
kevin ventura <kevcodes@gmail.com>
2016-06-21 18:47:57 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,54 @@
+def using_push(array,country)
+updated_array = []
+updated_array.push("Niger")
+
+end
+
+def using_unshift(array, neighborhood)
+  updated_array = []
+  array.unshift(neighborhood)
+
+  end
+
+def using_pop(great_hits_of_the_nineties )
+  great_hits_of_the_nineties.pop
+end
+
+def pop_with_args(chars_in_game_of_thrones)
+  chars_in_game_of_thrones.pop(2)
+  end
+
+  def using_shift(my_favorite_cities)
+    my_favorite_cities.shift
+  end
+
+  def shift_with_args(ice_cream_brands)
+  ice_cream_brands.shift(2)
+end
+
+def using_concat(my_favorite_things, more_favs)
+
+  my_favorite_things.concat(more_favs)
+end
+
+def using_insert(list_of_esoteric_programming_languages, another_esoteric_language)
+  list_of_esoteric_programming_languages.insert(4, another_esoteric_language)
+  end
+
+  def using_uniq(captain_planet_and_the_planeteers)
+    captain_planet_and_the_planeteers.uniq
+  end
+
+def using_flatten(private_colleges_in_newyork)
+  private_colleges_in_newyork.flatten
+end
+
+
+
+def using_delete(instructors, steven)
+  instructors.delete(steven)
+end
+
+def using_delete_at(famous_robots, deleted_robot)
+  famous_robots.delete_at(2)
+  end
